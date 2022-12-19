8650a1c7ab462a337e5dee8540c3b25d9c1fc0a9
kumawatlalit912 <kumawatlalit912@gmail.com>
2022-12-19 16:54:26 +0530

    updated

---
+++ frontend/src/store.js
@@ -0,0 +1,48 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+
+//kind of middleware -->thunk
+
+import thunk from "redux-thunk";
+//download same extension in chrome as well
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  noteCreateReducer,
+  noteDeleteReducer,
+  noteListReducer,
+  noteUpdateReducer,
+} from "./reducers/notesReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateReducer,
+} from "./reducers/userReducers";
+
+const reducer = combineReducers({
+  noteList: noteListReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  noteCreate: noteCreateReducer,
+  noteDelete: noteDeleteReducer,
+  noteUpdate: noteUpdateReducer,
+  userUpdate: userUpdateReducer,
+});
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
+
+const middleware = [thunk];
+
+//createStore is now deprecated you can use configureStore instead
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
